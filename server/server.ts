71cc93b0b4d02678d6cb59b113ad4f92953a33ff
Will Zich <will@zich.wtf>
2024-12-15 23:11:03 -0800

    create game and get board routes

--- server/server.ts
+++ server/server.ts
@@ -3,33 +3,83 @@ import { Database } from "jsr:@db/sqlite";
 
 const app = new Hono();
 const db = new Database(":memory:");
+
+// Create GAME with cols GAME_ID, NAME
 db.exec(
-  "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null, tiles TEXT not null)",
+  "CREATE TABLE IF NOT EXISTS game (game_id STRING PRIMARY KEY, name TEXT not null)",
+);
+// Create TILE with cols TILE_ID, GAME_ID, VALUE, COMPLETED
+db.exec(
+  "CREATE TABLE IF NOT EXISTS tile (tile_id INTEGER PRIMARY KEY AUTOINCREMENT, game_id INTEGER not null, value TEXT not null, completed BOOLEAN)",
+);
+// Create PLAYER with cols PLAYER_ID, NAME
+db.exec(
+  "CREATE TABLE IF NOT EXISTS player (player_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null)",
+);
+// Create BOARD with cols BOARD_TILE_ID, GAME_ID, BOARD_ID, PLAYER_ID, TILE_ID
+db.exec(
+  "CREATE TABLE IF NOT EXISTS board (board_tile_id INTEGER PRIMARY KEY AUTOINCREMENT, game_id STRING not null, board_id STRING not null, player_id INTEGER not null, tile_id INTEGER not null)",
 );
 
-app.get("/", (c) => c.text("Hello, World!"));
-app.post("/create-game", async (c) => {
+app.post("/game", async (c) => {
   const body = await c.req.json();
-  console.log(body);
-  const stmt = db.prepare(
-    "INSERT INTO games (name, tiles) VALUES (:name, :tiles)",
-  );
-  const changes = stmt.all({
-    name: crypto.randomUUID().substring(0, 8),
-    tiles: JSON.stringify(body.tiles),
-  });
-  return changes ? c.json({ status: "success" }) : c.json({ status: "failed" });
+  if (body.tiles.length < 16) {
+    return c.json({
+      "status": "failed",
+      "message": "Please provide at least 16 options",
+    });
+  }
+  const tiles = body.tiles;
+  const game_id = crypto.randomUUID().substring(0, 8);
+  const name = game_id;
+  const make_game = db.prepare(
+    "INSERT INTO game (game_id, name) VALUES (:game_id, :name)",
+  ).all({ game_id, name });
+  db.transaction(() => {
+    for (const tile of tiles) {
+      db.prepare(
+        "INSERT INTO tile (game_id, value, completed) VALUES (:game_id, :tile, 0)",
+      ).all({ game_id, tile });
+    }
+  })();
+
+  return make_game
+    ? c.json({ status: "success", game_id })
+    : c.json({ status: "failed" });
 });
 
-app.get("/games", (c) => {
-  const stmt = db.prepare("SELECT id, name, tiles FROM games");
-  const rows = stmt.all();
-  console.log(rows);
-  const response = rows.map((row) => ({
-    ...row,
-    tiles: JSON.parse(row.tiles),
-  }));
-  return c.json(response);
+app.get("/board/:game_name/:player_id", async (c) => {
+  const name = await c.req.param("game_name");
+  const player_id = await c.req.param("player_id");
+
+  function getBoard({ name, player_id }: { name: string; player_id: string }) {
+    return db.prepare(
+      "SELECT * FROM board INNER JOIN tile WHERE board.game_id = :name AND board.player_id = :player_id",
+    ).all({ name, player_id });
+  }
+
+  const existing_board = getBoard({ name, player_id });
+  if (existing_board.length != 0) {
+    return c.json(existing_board);
+  }
+
+  const board_id = crypto.randomUUID().substring(0, 8);
+  db.prepare(
+    "INSERT INTO board (board_id, game_id, player_id, tile_id) SELECT :board_id, :name, :player_id, tile_id FROM tile INNER JOIN game ON game.game_id = tile.game_id WHERE game.name = :name ORDER BY RANDOM() LIMIT 16;",
+  ).all({ board_id, player_id, name });
+  const new_board = getBoard({ name, player_id });
+  return c.json(new_board);
+});
+
+// Debug routes
+app.get("/list-tiles", (c) => {
+  const stmt = db.prepare("SELECT * FROM tile").all();
+  return c.json(stmt);
+});
+
+app.get("/list-games", (c) => {
+  const games = db.prepare("SELECT * FROM game").all();
+  return c.json(games);
 });
 
 Deno.serve(app.fetch);
