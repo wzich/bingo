a5e79c3aa5fa2322a1531656eb188622efaab69e
Will Zich <will@zich.wtf>
2024-12-30 14:08:11 -0600

    cors + fixing board creation sql

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { Hono } from "jsr:@hono/hono";
+import { cors } from "jsr:@hono/hono/cors";
 import { Database } from "jsr:@db/sqlite";
 
 const app = new Hono();
@@ -18,8 +19,10 @@ db.exec(
 );
 // Create BOARD with cols BOARD_TILE_ID, GAME_ID, BOARD_ID, PLAYER_ID, TILE_ID
 db.exec(
-  "CREATE TABLE IF NOT EXISTS board (board_tile_id INTEGER PRIMARY KEY AUTOINCREMENT, game_id STRING not null, board_id STRING not null, player_id INTEGER not null, tile_id INTEGER not null)",
+  "CREATE TABLE IF NOT EXISTS board (game_id STRING not null, board_id STRING not null, player_id INTEGER not null, tile_id INTEGER not null)",
 );
+
+app.use("/*", cors());
 
 app.post("/game", async (c) => {
   const body = await c.req.json();
@@ -48,30 +51,37 @@ app.post("/game", async (c) => {
     : c.json({ status: "failed" });
 });
 
-app.get("/board/:game_name/:player_id", async (c) => {
-  const name = await c.req.param("game_name");
+app.get("/board/:game_id/:player_id", async (c) => {
+  const game_id = await c.req.param("game_id");
   const player_id = await c.req.param("player_id");
 
-  function getBoard({ name, player_id }: { name: string; player_id: string }) {
+  function getBoard(
+    { game_id, player_id }: { game_id: string; player_id: string },
+  ) {
     return db.prepare(
-      "SELECT * FROM board INNER JOIN tile WHERE board.game_id = :name AND board.player_id = :player_id",
-    ).all({ name, player_id });
+      "SELECT * FROM board INNER JOIN tile ON board.tile_id = tile.tile_id WHERE board.game_id = :game_id AND board.player_id = :player_id",
+    ).all({ game_id, player_id });
   }
 
-  const existing_board = getBoard({ name, player_id });
+  const existing_board = getBoard({ game_id, player_id });
   if (existing_board.length != 0) {
     return c.json(existing_board);
   }
 
   const board_id = crypto.randomUUID().substring(0, 8);
   db.prepare(
-    "INSERT INTO board (board_id, game_id, player_id, tile_id) SELECT :board_id, :name, :player_id, tile_id FROM tile INNER JOIN game ON game.game_id = tile.game_id WHERE game.name = :name ORDER BY RANDOM() LIMIT 16;",
-  ).all({ board_id, player_id, name });
-  const new_board = getBoard({ name, player_id });
-  return c.json(new_board);
+    "INSERT INTO board (board_id, game_id, player_id, tile_id) SELECT :board_id, :game_id, :player_id, tile_id FROM tile INNER JOIN game ON game.game_id = tile.game_id WHERE game.game_id = :game_id ORDER BY RANDOM() LIMIT 16;",
+  ).all({ board_id, player_id, game_id });
+  const player_board = getBoard({ game_id, player_id });
+  return c.json(player_board);
 });
 
 // Debug routes
+app.get("/show-boards", (c) => {
+  const response = db.prepare("SELECT * FROM board").all();
+  return c.json(response);
+});
+
 app.get("/list-tiles", (c) => {
   const stmt = db.prepare("SELECT * FROM tile").all();
   return c.json(stmt);
